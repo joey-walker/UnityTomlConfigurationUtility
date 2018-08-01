8e2f96d2a1a109aed360c6930f764bd5ddf13111
Joseph Walker <star20@comcast.net>
2018-08-01 09:34:44 -0400

    Start Audio side

---
+++ Audio.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace ZCorp.ConfigurationUtilities
+{
+    public class Audio
+    {
+        public Audio()
+        {
+            SampleRate = AudioSettings.GetConfiguration().sampleRate;
+            DspBufferSize = AudioSettings.GetConfiguration().dspBufferSize;
+            AudioSpeakerMode = AudioSettings.GetConfiguration().speakerMode;
+            Volume = AudioListener.volume;
+        }
+
+        public int SampleRate { get; set; }
+        public int DspBufferSize { get; set; }
+        public AudioSpeakerMode AudioSpeakerMode { get; set; }
+        public float Volume { get; set; }
+    }
+}
--- BaseConfiguration.cs
+++ BaseConfiguration.cs
@@ -2,12 +2,13 @@
 {
     public class BaseConfiguration
     {
-
         public BaseConfiguration()
         {
-         Graphics = new Graphics();
+            Graphics = new Graphics();
+            Audio = new Audio();
         }
 
-        public Graphics Graphics { set; get;  }
+        public Graphics Graphics { set; get; }
+        public Audio Audio { set; get; }
     }
 }
